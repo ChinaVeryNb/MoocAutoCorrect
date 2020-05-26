38e06496feae968b5e1d26192318fc5d1de7ab89
jiaguo <56522923+ChinaVeryNb@users.noreply.github.com>
2020-05-26 20:53:03 +0800

    Create AutoCorrect.js

---
+++ AutoCorrect.js
@@ -0,0 +1,25 @@
+sum = prompt("请输入题目数量：");
+grade = prompt("请输入要打的分数：");
+comment = prompt("请输入评价：")
+value = document.getElementsByClassName("j-textarea inputtxt")[1].attributes[2].value;
+num = parseInt(value.slice(8));
+for (var i = 0; i < 20 * sum; i++) {
+    try {
+        a = document.getElementById("auto-id-" + num.toString());
+        a.value = comment;
+        console.log(i);
+    } catch (error) {
+    }
+    num++;
+}
+list = document.getElementsByClassName("s");
+console.log(list.length);
+for (var i = 0; i < list.length; i++) {
+    try {
+        var list2 = list[i].getElementsByClassName("d");
+        console.log(list2[10].getElementsByClassName("j-select")[0].attributes[1].value);
+        list2[grade].getElementsByClassName("j-select")[0].checked = true;
+    } catch (errror) {
+        console.log(i + "!");
+    }
+}
